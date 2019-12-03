28378a5164ce96814c10d4b486cc47306ad84c18
András Ács <acsandras@gmail.com>
2019-12-03 13:57:21 +0100

    Udvikler musesspil trin 2

--- src/mus/MuseSpil.java
+++ src/mus/MuseSpil.java
@@ -6,16 +6,32 @@
  */
 package mus;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MuseSpil {
 
     // Mus begynder med at der genereres et antal pebernødder (ca. 60-120)
     public int minNuts = 60;
     public int maxNuts = 120;
     public int antalNuts;
+    private int valgteNut;
 
     public void generateNuts() {
-        antalNuts = (int) Math.random() * maxNuts - minNuts;
+        // FORKERT antalNuts = (int) Math.random() * maxNuts - minNuts;
+        // KORREKT antalNuts = ThreadLocalRandom.current().nextInt(minNuts, maxNuts + 1);
+        // TAK Anders
+        Random random = new Random();
+        antalNuts = random.nextInt(maxNuts-minNuts)+minNuts;
         System.out.println(antalNuts + " nødder er lavet.");
+    }
+
+    // Computeren vælger en pebernød.
+    protected int chooseNut() {
+        Random random = new Random();
+        valgteNut = random.nextInt(antalNuts-1)+1;
+        System.out.println( "Den valgte nød er " + valgteNut);
+        return valgteNut;
     }
 
 }
--- src/mus/MuseSpilTest.java
+++ src/mus/MuseSpilTest.java
@@ -4,4 +4,19 @@ import junit.framework.TestCase;
 
 public class MuseSpilTest extends TestCase {
 
+
+    public void testGenerateNuts() {
+
+        MuseSpil spil = new MuseSpil();
+        spil.generateNuts();
+
+    }
+
+    public void testValgNut() {
+
+        MuseSpil spil = new MuseSpil();
+        spil.generateNuts();
+        spil.chooseNut();
+
+    }
 }
